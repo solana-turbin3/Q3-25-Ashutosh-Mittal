50fefd2c9fd39d3ac56f13fda05e7c2e066d5d69
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2025-07-09 18:19:17 +0530

    vault deposit add

--- vault/programs/vault/src/lib.rs
+++ vault/programs/vault/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(unexpected_cfgs)]
 #![allow(deprecated)]
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, system_program::{transfer, Transfer}};
 
 declare_id!("Aww3qCodkpqeVb2akSBLumb8SVYkSSGbPrV14NJZ9KnJ");
 
@@ -10,8 +10,12 @@ pub mod vault {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         // msg!("Greetings from: {:?}", ctx.program_id);
-        
-        Ok(())
+        ctx.accounts.initialize(&ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        // msg!("Greetings from: {:?}", ctx.program_id);
+        ctx.accounts.deposit(amount)
     }
 }
 
@@ -51,6 +55,50 @@ impl <'info> Initialize <'info>{
         Ok(())
     }
 }
+
+
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub signer: Signer <'info>,
+    #[account(
+mut, 
+seeds = [b"vault",vault_state.key().as_ref()],
+bump = vault_state.vault_bump,
+
+    )]  
+        pub vault: SystemAccount <'info>,
+ #[account(
+seeds = [b"state",signer.key().as_ref()],
+bump =  vault_state.state_bump,
+
+    )]  
+
+    pub vault_state: Account <'info, VaultState>,
+        pub system_program: Program<'info, System>,
+
+}
+
+
+impl <'info> Deposit <'info>{
+    pub fn deposit (&mut self, amount: u64)-> Result<()>{
+
+       let cpi_program = self.system_program.to_account_info();
+
+       let cpi_account = Transfer{
+        from: self.signer.to_account_info(),
+        to: self.vault.to_account_info()
+       };
+
+       let cpi_ctx = CpiContext::new(cpi_program, cpi_account);
+
+       transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+}
+
 
 
 #[account]
